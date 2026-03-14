dd63237ed6eaa3d6604edecb754f3b732e98666a
maktaba-johir <muftijohirulislam2020@gmail.com>
2026-03-14 22:19:28 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'rahbar-cache-v2';
+const assets = [
+  './',
+  './index.html',
+  './icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;600;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
